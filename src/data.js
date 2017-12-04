6135a2a33ae3b7d6f7aa6a387a304999686c32f6
Ex-iT <vic.toothpick@gmail.com>
2017-12-05 00:27:35 +0100

    Various small fixes

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ module.exports = {
 		title: 'Home',
 		body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam temporibus provident voluptatem architecto nesciunt eaque delectus facilis distinctio nostrum magnam illo, dolorum iure, in excepturi alias ducimus velit inventore! Corporis!Ipsam libero, iusto ea aspernatur dolorem illo optio laborum nobis quod, maiores maxime, earum ducimus suscipit minus modi ullam? Tempore, praesentium harum. Reiciendis nemo praesentium temporibus eius incidunt aut cupiditate?Debitis quo quasi voluptate, enim doloremque ut ratione voluptatem harum itaque neque in libero, et quis recusandae! Quibusdam, repellendus saepe. Numquam, fuga aliquid. Dolorum excepturi hic nulla mollitia modi facilis.Ipsam asperiores assumenda sit ea et aliquid consectetur perferendis explicabo debitis corrupti nihil minus deserunt, officia, itaque iste? Rerum esse, id magnam eaque dolorum cumque asperiores autem non eos aperiam!Facilis, id libero. Voluptates ducimus rem at voluptatem inventore eaque distinctio porro eligendi incidunt! Atque magnam dolorum nobis blanditiis reprehenderit velit deserunt, eligendi qui, harum earum enim maiores iste. Odit.Perspiciatis iste error repudiandae nihil deleniti. Explicabo harum error nesciunt ipsa qui sunt dolorem voluptates iure vero dolores dolore modi perspiciatis quod tempora necessitatibus odit doloribus, quidem praesentium obcaecati. Nisi!Assumenda, officia! Facilis earum et porro, consequuntur tempora atque? Deleniti laborum totam ad. Corporis accusantium ad alias temporibus id ullam mollitia dolorum labore, porro deserunt in pariatur beatae, iusto maiores!At cum tempora accusantium voluptatem, optio temporibus itaque facere, eligendi nostrum nam sint dolore. Explicabo iusto facere molestiae laborum dicta mollitia, fugit possimus pariatur? Sunt deleniti neque esse quod molestias.Quasi sapiente odit deserunt? Atque aspernatur cupiditate nisi praesentium modi laudantium voluptate maiores harum quos consequatur porro cum repellendus quam alias, blanditiis labore cumque necessitatibus delectus eligendi, laborum quis similique?Molestias optio libero, obcaecati officia totam architecto iure fugiat non eligendi delectus laboriosam beatae dolores corrupti. Saepe mollitia ducimus architecto rerum, optio qui libero veritatis necessitatibus blanditiis culpa eos deserunt!Reprehenderit aperiam quasi, eveniet eligendi aliquam minus distinctio culpa alias. Laboriosam sint impedit maxime nihil vel quas amet nostrum labore delectus commodi necessitatibus debitis tempora, ipsam eos! Fuga, earum id.Animi alias velit nisi itaque eum laborum soluta repudiandae sequi nam amet obcaecati eaque maiores eos eveniet ad, natus recusandae pariatur cumque excepturi ab nulla. Veniam vero quo voluptatibus ea?Quod dolorem nesciunt ipsum. Temporibus, nam laboriosam odio sapiente architecto ab ex optio blanditiis consequuntur aliquam nobis aperiam nesciunt aspernatur! Inventore excepturi saepe iure ut sapiente illo modi quas culpa.Maiores exercitationem labore veniam rerum esse explicabo cum, sapiente nemo sequi ad ducimus aperiam. Eaque, maiores eveniet saepe earum voluptate, atque illum tenetur error totam fuga ratione similique iure quibusdam.Repellat odit ad odio dolore maxime nam numquam porro sed saepe rerum repellendus tempora, nostrum vitae dolores dolorum fuga. Ipsa quaerat consequuntur mollitia reprehenderit reiciendis explicabo dolores dicta soluta minus!Quos commodi in necessitatibus dignissimos, perferendis doloremque voluptate obcaecati unde possimus inventore corrupti quasi, aliquid facere illo dicta reiciendis nesciunt temporibus quibusdam aliquam facilis earum. Quo excepturi adipisci corporis explicabo!Magnam optio delectus amet, excepturi doloribus alias autem eveniet quas accusamus iure, velit consectetur aspernatur! Delectus, culpa, reiciendis eveniet a recusandae eum itaque nobis saepe numquam dolor nisi dolore inventore?Quae, temporibus! Quo molestias voluptatum, praesentium vero soluta alias distinctio consectetur adipisci, omnis nesciunt iste explicabo quibusdam maxime est libero eius assumenda quia fuga dignissimos. Laboriosam deserunt quas voluptate. Laborum.Qui ex est maiores temporibus, laboriosam tempora quos accusamus quasi mollitia nam, excepturi voluptatum iste eius consequuntur quod maxime dolores ipsum a! Iure eum animi fugiat corporis minus sequi facere.Error saepe odio hic quia voluptatum quas mollitia recusandae nobis sint cupiditate illo animi, necessitatibus vero explicabo aliquam atque magnam libero eveniet dignissimos aut facilis consequatur provident id vel. Aspernatur?',
 		meta: {
-			keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, ontruiming, ondersteuning',
+			keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, huisontruiming, ondersteuning',
 			description: 'Promink Seniorenverhuizing - Van huis naar thuis.'
 		}
 	},
@@ -16,7 +16,7 @@ module.exports = {
 			title: 'Diensten op maat',
 			body: '2343222424',
 			meta: {
-				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, ontruiming, ondersteuning',
+				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, huisontruiming, ondersteuning',
 				description: 'Promink Seniorenverhuizing - Van huis naar huis.'
 			}
 		},
@@ -24,16 +24,16 @@ module.exports = {
 			title: 'Opruimen oude huis',
 			body: '2343222424',
 			meta: {
-				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, ontruiming, ondersteuning',
+				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, huisontruiming, ondersteuning',
 				description: 'Promink Seniorenverhuizing - Van huis naar huis.'
 			}
 		},
 		{
-			title: 'Ontruiming appartement met spoed?',
+			title: 'huisontruiming appartement met spoed?',
 			body: '2343222424',
 			special: true,
 			meta: {
-				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, ontruiming, ondersteuning',
+				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, huisontruiming, ondersteuning',
 				description: 'Promink Seniorenverhuizing - Van huis naar huis.'
 			}
 		},
@@ -41,7 +41,7 @@ module.exports = {
 			title: 'Inrichten nieuwe huis',
 			body: '2343222424',
 			meta: {
-				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, ontruiming, ondersteuning',
+				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, huisontruiming, ondersteuning',
 				description: 'Promink Seniorenverhuizing - Van huis naar huis.'
 			}
 		},
@@ -49,7 +49,7 @@ module.exports = {
 			title: 'Technische ondersteuning',
 			body: '<ul><li>Schilderen, behangen, stofferen, lampen en schilderijen ophangen.</li></ul>',
 			meta: {
-				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, ontruiming, ondersteuning',
+				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, huisontruiming, ondersteuning',
 				description: 'Promink Seniorenverhuizing - Van huis naar huis.'
 			}
 		},
@@ -57,7 +57,7 @@ module.exports = {
 			title: 'Wijziging aansluitingen / abonnementen',
 			body: '<ul><li>Met wie heeft u contacten, en vooral contracten..</li></ul>',
 			meta: {
-				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, ontruiming, ondersteuning',
+				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, huisontruiming, ondersteuning',
 				description: 'Promink Seniorenverhuizing - Van huis naar huis.'
 			}
 		},
@@ -65,7 +65,7 @@ module.exports = {
 			title: 'Wij werken samen met:',
 			body: 'logo\'s + links',
 			meta: {
-				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, ontruiming, ondersteuning',
+				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, huisontruiming, ondersteuning',
 				description: 'Promink Seniorenverhuizing - Van huis naar huis.'
 			}
 		},
@@ -73,7 +73,7 @@ module.exports = {
 			title: 'Referenties:',
 			body: 'logo\'s, links + text',
 			meta: {
-				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, ontruiming, ondersteuning',
+				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, huisontruiming, ondersteuning',
 				description: 'Promink Seniorenverhuizing - Van huis naar huis.'
 			}
 		},
@@ -81,7 +81,7 @@ module.exports = {
 			title: 'Nevenactiviteiten:',
 			body: 'logo\'s, links + text',
 			meta: {
-				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, ontruiming, ondersteuning',
+				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, huisontruiming, ondersteuning',
 				description: 'Promink Seniorenverhuizing - Van huis naar huis.'
 			}
 		},
@@ -89,7 +89,7 @@ module.exports = {
 			title: 'Fotoimpressie:',
 			body: 'Pictures + labels',
 			meta: {
-				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, ontruiming, ondersteuning',
+				keywords: 'Verhuizing, geplande verhuizing, seniorenverhuizing, huisontruiming, ondersteuning',
 				description: 'Promink Seniorenverhuizing - Van huis naar huis.'
 			}
 		}
@@ -126,7 +126,7 @@ module.exports = {
 		'pb.html': '/persoonlijke-begeleiding/',
 		'dom.html': '/diensten-op-maat/',
 		'ooh.html': '/opruimen-oude-huis/',
-		'oams.html': '/ontruiming-appartement-met-spoed/',
+		'oams.html': '/huisontruiming-appartement-met-spoed/',
 		'inh.html': '/inrichten-nieuwe-huis/',
 		'to.html': '/technische-ondersteuning/',
 		'waa.html': '/wijziging-aansluitingen-/-abonnementen/',
